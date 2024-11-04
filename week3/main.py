8553a356efafadaf860f109b07f65a990ede1b7a
Phat Truong <phat.truong@knorex.com>
2024-11-04 01:56:47 +0100

    final work for explicit gd

--- week3/main.py
+++ week3/main.py
@@ -1,42 +1,67 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.ndimage import generic_filter
+from segmentation import initialize_level_set, _level_set_gradient, compute_binaryvalue_c1c2
+import sys
 
-folderInput = 'images/'
-figure_name = 'phantom2.bmp'
-figure_name_final = folderInput + figure_name
-img = cv2.imread(figure_name_final, cv2.IMREAD_UNCHANGED)
-img = cv2.bilateralFilter(img,11,75,75)
+# default parameters
+mu = 1
+nu = 0.045
+lambda1 = 1
+lambda2 = 1
+tol = 1e-4
+dt = 0.5
+iterMax = 3e4
+
+if sys.argv[1] == 'phantom3':
+    folderInput = 'images/'
+    figure_name = 'phantom3.bmp'
+    figure_name_final = folderInput + figure_name
+    img = cv2.imread(figure_name_final, cv2.IMREAD_UNCHANGED)
+    img = generic_filter(img, np.var, size=11)
+    img = cv2.GaussianBlur(img,(11,11),0)
+
+    mu = 1
+    nu = 0.045
+    lambda1 = 1
+    lambda2 = 1
+    tol = 1e-4
+    dt = 0.5
+    iterMax = 3e4
+    epsilon = 1.5
+elif sys.argv[1] == 'phantom2':
+    folderInput = 'images/'
+    figure_name = f'{sys.argv[1]}.bmp'
+    figure_name_final = folderInput + figure_name
+    img = cv2.imread(figure_name_final, cv2.IMREAD_UNCHANGED)
+    img = cv2.bilateralFilter(img,11,75,75)
+    epsilon = 1.5
+elif sys.argv[1] == 'phantom1':
+    folderInput = 'images/'
+    figure_name = f'{sys.argv[1]}.bmp'
+    figure_name_final = folderInput + figure_name
+    img = cv2.imread(figure_name_final, cv2.IMREAD_UNCHANGED)
+    mu = 0.2
+    nu = 0.001
+    epsilon = 1
+else: 
+    folderInput = 'images/'
+    figure_name = f'circles.png'
+    figure_name_final = folderInput + figure_name
+    img = cv2.imread(figure_name_final, cv2.IMREAD_UNCHANGED)
+    mu = 0.2
+    nu = 0.001
+    epsilon = 1
+    
 img = img.astype('float')
 
 # Visualize the grayscale image
-# cv2.imshow('Image', img); cv2.waitKey(0)
+cv2.imshow('Image', img); cv2.waitKey(0)
 
 # Normalize image
 img = (img.astype('float') - np.min(img))
 img = img/np.max(img)
 cv2.imshow('Normalized image',img); cv2.waitKey(0)
-
-# f = np.fft.fft2(img)
-# fshift = np.fft.fftshift(f)
-# m,n = fshift.shape
-# zeros = np.zeros_like(fshift)
-# s= 50
-# zeros[int(m/2)-s:int(m/2)+s, int(n/2)-s:int(n/2)+s] = fshift[int(m/2)-s:int(m/2)+s, int(n/2)-s:int(n/2)+s] 
-# fshift = zeros
-# magnitude_spectrum = np.log1p(np.abs(fshift))
- 
-# plt.subplot(121),plt.imshow(img, cmap = 'gray')
-# plt.title('normalized im'), plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-# plt.title('mag'), plt.xticks([]), plt.yticks([])
-# plt.show()
-
-# f_ishift = np.fft.ifftshift(fshift)
-# img = np.fft.ifft2(f_ishift)
-# img = np.real(img)
-# cv2.imshow('Image', img); cv2.waitKey(0)
-
 # Height and width
 ni = img.shape[0]
 nj = img.shape[1]
@@ -46,64 +71,20 @@ if len(img.shape) > 2:
     nc = img.shape[2] # number of channels
     img = np.mean(img, axis=2)
 
-# Try out different parameters
-mu = 1
-nu = 0.045
-lambda1 = 1
-lambda2 = 1
-tol = 1e-4
-dt = 0.5
-iterMax = 3e4
-
-X, Y = np.meshgrid(np.arange(0, nj), np.arange(0, ni), indexing='xy')
-
 # Initial phi
 # This initialization allows a faster convergence for phantom2
-phi = (-np.sqrt((X - np.round(ni / 2)) ** 2 + (Y - np.round(nj / 2)) ** 2) + 50)
-# print(X.shape, Y.shape)
+phi = initialize_level_set((ni,nj))
 # Alternatively, you may initialize randomly, or use the checkerboard pattern as suggested in Getreuer's paper
-
-# Normalization of the initial phi to the range [-1, 1]
-min_val = np.min(phi)
-max_val = np.max(phi)
-phi = phi - min_val
-phi = 2 * phi / max_val
-phi = phi - 1
 
 # CODE TO COMPLETE
 # Explicit gradient descent or Semi-explicit (Gauss-Seidel) gradient descent (Bonus)
 # Extra: Implement the Chan-Sandberg-Vese model (for colored images)
 # Refer to Getreuer's paper (2012)
 
-# CODE TO COMPLETE
-epsilon = 1.5
 
 for iter in range(int(iterMax)):
-    # heaviside function H(phi(x))
-    H_phi_x = 0.5 * (1 + (2 / np.pi) * np.arctan(phi / epsilon))
-
-    # compute c1 and c2
-    c1 = np.sum(H_phi_x * img) / (np.sum(H_phi_x) + 1e-10) #tiny number to avoid zero division
-    c2 = np.sum((1 - H_phi_x) * img) / (np.sum(1 - H_phi_x) + 1e-10)
-
-    # compute delta_phi_x
-    delta_phi_x = (epsilon / (np.pi * (epsilon**2 + phi**2)))
-
-    # compute gradients of phi
-    phi_y, phi_x = np.gradient(phi)
-
-    # compute gradient magnitude
-    grad_phi_mag = np.sqrt(phi_x**2 + phi_y**2) + 1e-10  
-
-    # normalized gradients
-    nx = phi_x / grad_phi_mag
-    ny = phi_y / grad_phi_mag
-
-    # Compute curvature
-    nxx = np.gradient(nx, axis=1)
-    nyy = np.gradient(ny, axis=0)
-    curvature = nxx + nyy
-    dphi_dt = delta_phi_x * (mu * curvature - nu - lambda1 * (img - c1)**2 + lambda2 * (img - c2)**2)
+    c1,c2 = compute_binaryvalue_c1c2(f= img, phi= phi, epsilon= epsilon)
+    dphi_dt = _level_set_gradient(phi, img, c1, c2,mu,nu,lambda1,lambda2,epsilon)
 
     # update phi
     phi_old = phi.copy()
--- week3/segmentation.py
+++ week3/segmentation.py
@@ -1,23 +1,45 @@
 import numpy as np
 
-def get_level_set(shape, type='default'):
-    Y, X = np.meshgrid(np.arange(0, shape[0]), np.arange(0, shape[1]), indexing='ij')
+def initialize_level_set(shape, type='default'):
+    X, Y = np.meshgrid(np.arange(0, shape[0]), np.arange(0, shape[1]), indexing='xy')
     if type == 'default':
-        return (-np.sqrt((Y - np.round(shape[0] / 2)) ** 2 + (X - np.round(shape[1] / 2)) ** 2) + 50)
+        phi = (-np.sqrt((X - np.round(shape[0] / 2)) ** 2 + (Y - np.round(shape[1] / 2)) ** 2) + 50)
     elif type == 'checkers':
-        return np.sin(np.pi/5*Y) * np.sim(np.pi/5*X)
-    return (-np.sqrt((Y - np.round(shape[0] / 2)) ** 2 + (X - np.round(shape[1] / 2)) ** 2) + 50)
+        phi = np.sin(np.pi/5*Y) * np.sim(np.pi/5*X)
+    else:
+        phi = (-np.sqrt((X - np.round(shape[0] / 2)) ** 2 + (Y - np.round(shape[1] / 2)) ** 2) + 50)
+    # Normalization of the initial phi to the range [-1, 1]
+    min_val = np.min(phi)
+    max_val = np.max(phi)
+    phi = phi - min_val
+    phi = 2 * phi / max_val
+    phi = phi - 1
+    return phi
 
-def _dirac_function(phi, epsilon):
-    return epsilon / (np.pi * (phi**2 + epsilon**2))
+def _dirac_delta(phi, epsilon):
+    return (epsilon / (np.pi * (phi**2 + epsilon**2)))
 
 
-def _level_set_gradient(phi, f, c1, c2, mu, nu, l1, l2):
+def compute_binaryvalue_c1c2(f, phi, epsilon):
+    H_phi_x = 0.5 * (1 + (2 / np.pi) * np.arctan(phi / epsilon))
+
+    # compute c1 and c2
+    c1 = np.sum(H_phi_x * f) / (np.sum(H_phi_x) + 1e-10) #tiny number to avoid zero division
+    c2 = np.sum((1 - H_phi_x) * f) / (np.sum(1 - H_phi_x) + 1e-10)
+    return c1,c2
+
+def _level_set_gradient(phi, f, c1, c2, mu, nu, l1, l2, epsilon):
     # Length Term Derivative
-    grad_x, grad_y = np.gradient(phi)
-    magnitude = np.sqrt(grad_x**2 + grad_y**2)
-    norm_grad_x = grad_x / (magnitude + 1e-10)
-    norm_grad_y = grad_y / (magnitude + 1e-10)
+    phi_y, phi_x = np.gradient(phi)
+
+    # compute gradient magnitude
+    grad_phi_mag = np.sqrt(phi_x**2 + phi_y**2) + 1e-10  
+
+    # normalized gradients
+    norm_grad_x = phi_x / grad_phi_mag
+    norm_grad_y = phi_y / grad_phi_mag
+
+    # compute curvature
     div_x = np.gradient(norm_grad_x, axis=1)
     div_y = np.gradient(norm_grad_y, axis=0)
     div = div_x + div_y
@@ -30,7 +52,7 @@ def _level_set_gradient(phi, f, c1, c2, mu, nu, l1, l2):
     # Area Term
     area_term = -nu
 
-    derivative = _dirac_function(phi, 1) * (length_term + area_term + region1_term + region2_term)
+    derivative = _dirac_delta(phi, epsilon) * (length_term + area_term + region1_term + region2_term)
     #TODO set image boudnary
     return derivative
     
